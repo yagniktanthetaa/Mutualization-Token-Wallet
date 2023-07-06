9fa48a60a04f5ce48f59f425e0c1b27107560b5c
yagniktanthetaa <yagnik@tanthetaa.co.in>
2023-07-06 09:39:43 +0530

    loading component are added

---
+++ src/components/common/ui/Loader/Loader.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <>
+      {/* <!-- SPINNER 13   --> */}
+      <div>
+        <div className="ml-loader">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
