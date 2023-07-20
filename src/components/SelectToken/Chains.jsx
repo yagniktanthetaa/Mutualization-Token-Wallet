2fcd5447af606fd5376e7d22196a8427a5137946
yagniktanthetaa <yagnik@tanthetaa.co.in>
2023-07-20 11:27:45 +0530

    add chains

--- src/components/SelectToken/Chains.jsx
+++ src/components/SelectToken/Chains.jsx
@@ -1,6 +1,37 @@
 import React, { useContext } from "react";
 import { SwapTokenContext } from "../context/SwapContext";
 
+export const ChainCard = (props) => {
+  const { chain, index, selectChains, handleSelectToken } = props;
+  return (
+    <>
+      <div
+        className="input-token-outer-layout cursor-pointer"
+        key={index}
+        onClick={() => handleSelectToken(chain.id)}
+      >
+        <div
+          className={`${
+            chain.id === selectChains ? "token-active" : "input-token-border"
+          }`}
+        >
+          <div className="flex-center flex-col">
+            <img
+              src={chain.logoURI}
+              alt={chain.name}
+              loading="lazy"
+              className="h-10 w-10 object-contain rounded-full"
+            />
+            <p className="mt-4 text-lg font-normal leading-5 text-white">
+              {chain.name}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 const Chains = () => {
   const { selectChains, setSelectChains, showMoreNetworks, chains } =
     useContext(SwapTokenContext);
@@ -14,58 +45,23 @@ const Chains = () => {
       <div className="grid grid-cols-4 gap-y-4 gap-x-2">
         {showMoreNetworks
           ? chains.map((chain, index) => (
-              <div
-                className="input-token-outer-layout cursor-pointer"
-                key={index}
-                onClick={() => handleSelectToken(chain.id)}
-              >
-                <div
-                  className={`${
-                    chain.id === selectChains
-                      ? "token-active"
-                      : "input-token-border"
-                  }`}
-                >
-                  <div className="flex-center flex-col">
-                    <img
-                      src={chain.logoURI}
-                      alt={chain.name}
-                      loading="lazy"
-                      className="h-10 w-10 object-contain rounded-full"
-                    />
-                    <p className="mt-4 text-lg font-normal leading-5 text-white">
-                      {chain.name}
-                    </p>
-                  </div>
-                </div>
-              </div>
+              <ChainCard
+                chain={chain}
+                index={index}
+                selectChains={selectChains}
+                handleSelectToken={handleSelectToken}
+              />
             ))
-          : chains.slice(0, 4).map((chain, index) => (
-              <div
-                className="input-token-outer-layout cursor-pointer"
-                key={index}
-                onClick={() => handleSelectToken(chain.id)}
-              >
-                <div
-                  className={`${
-                    chain.id === selectChains
-                      ? "token-active"
-                      : "input-token-border"
-                  }`}
-                >
-                  <div className="flex-center flex-col">
-                    <img
-                      src={chain.logoURI}
-                      alt={chain.name}
-                      className="h-10 w-10 object-contain rounded-full"
-                    />
-                    <p className="mt-4 text-lg font-normal leading-5 text-white">
-                      {chain.name}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
+          : chains
+              .slice(0, 4)
+              .map((chain, index) => (
+                <ChainCard
+                  chain={chain}
+                  index={index}
+                  selectChains={selectChains}
+                  handleSelectToken={handleSelectToken}
+                />
+              ))}
       </div>
     </>
   );
